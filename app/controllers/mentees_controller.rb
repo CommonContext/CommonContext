8a08701d6b2ea0cced56c7310e7621081ff42101
Rebecca Orr <wholemilklover@gmail.com>
2017-02-25 14:31:22 -0800

    Writes routes for mentee creation and has incomplete form.

---
+++ app/controllers/mentees_controller.rb
@@ -0,0 +1,40 @@
+class MenteesController < ApplicationController
+  before_action :set_mentee, only: [:show, :edit, :update, :destroy]
+
+  def new
+    @mentee = Mentee.new
+  end
+
+  def create
+    respond_to do |format|
+      if @mentee.save
+        format.html { redirect_to @mentee }
+        format.json { render :show, status: :created, location: @mentee }
+      else
+        format.html { render :new }
+        format.json { render json: @mentee.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+   private
+    def set_mentee
+      @mentee = Mentee.find(params[:id])
+    end
+
+    def mentee_params
+      params.require(mentee).permit(:name,
+                                    :email,
+                                    :objective,
+                                    :mentor_demographic_pref,
+                                    :race,
+                                    :gender,
+                                    :linkedin_link,
+                                    :socioeconomic_background,
+                                    :orientation,
+                                    :other,
+                                    :location,
+                                    :interests)
+      
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :mentors #TODO: Determine if we need to limit any of these routes
+  resources :mentees, only: [:create, :new, :edit, :show]
 
   root 'mentors#show'
 
