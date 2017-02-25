427de406aaf8ccbe0be10727cb6b9f7002d64a6d
Rebecca Orr <wholemilklover@gmail.com>
2017-02-24 16:18:31 -0800

    Develops mentor controller

---
+++ app/controllers/mentor_controller.rb
@@ -0,0 +1,65 @@
+class MentorController < ApplicationController
+  before_action :find_mentor, only: [:edit, :show, :destroy]
+
+  def index
+    @mentors = Mentor.all
+  end
+
+  def new
+    @mentor = Mentor.new
+  end
+
+  def create
+    @mentor = Mentor.new(mentor_params)
+
+    if @mentor.save
+      render json: @mentor
+    else
+      redirect_to 'new'
+    end
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @mentor.update(mentor_params)
+      render json: @mentor
+    else
+      redirect_to 'edit'
+    end
+  end
+
+  def destroy
+    @mentor.destroy
+    redirect_to root_path
+  end
+
+
+  private
+
+  def find_mentor
+    @mentor = Mentor.find(params[:id])
+  end
+
+  def mentor_params
+    params.require(mentor).permit(:name,
+                                  :email,
+                                  :years_of_experience,
+                                  :current_company,
+                                  :current_industry,
+                                  :linkedin_link,
+                                  :race,
+                                  :gender,
+                                  :socioeconomic_background,
+                                  :orientation,
+                                  :location,
+                                  :interests,
+                                  :availability,
+                                  :other)
+  end
+
+end
