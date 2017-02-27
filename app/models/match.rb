d6feefabdd4d39aad1d7e96d7be3cdfcc3853183
Rebecca Orr <wholemilklover@gmail.com>
2017-02-27 00:09:56 -0800

    Adds mentor and mentee profile, mentee match request, mentee appointment booking with super mvp functionality.

--- app/models/match.rb
+++ app/models/match.rb
@@ -2,4 +2,31 @@ class Match < ApplicationRecord
   belongs_to :mentor
   belongs_to :mentee
 
+# TODO: refactor. Algorithm depends on default fields
+# existing on the mentee and mentor model if they haven't been specified
+# we also probably won't want to load our entire mentor table everytime this
+# is called
+
+  def mentor_matches(mentee)
+    mentors = Mentor.all 
+    matches = []
+    
+    # Coding out while loop for faster debugging purposes, 
+    # though for refactor it's important to consider cases
+    # where no matches are found
+    # while matches.length <= 1
+      mentors.each do |mentor|
+        if mentor.race == mentee.race
+          matches.push(mentor)
+        elsif mentor.socioeconomic_background == mentee.socioeconomic_background
+          matches.push(mentor)
+        elsif mentor.gender == mentee.gender
+          matches.push(mentor)
+        elsif mentor.orientation == mentee.orientation
+          matches.push(mentor)
+        end
+      end
+    # end
+    matches
+  end
 end
