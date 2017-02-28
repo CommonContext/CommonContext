1e141f05e244280d6e95f89be3fd8e6012e496be
Jack Dagley <jack.dagley77@gmail.com>
2017-02-28 11:09:23 -0800

    Adds mentor to seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,6 +11,10 @@ User.connection.execute('ALTER SEQUENCE users_id_seq RESTART WITH 1')
 
 
 r = Mentor.create!(first_name: "Andrea", last_name: "James",email: "a@mail.com", years_of_experience: 5, current_company: "DBC", current_industry: "Education", linkedin_link: "linkedin.com/andrea", race: "Asian", gender: "female", socioeconomic_background: "lower", orientation: "bisexual", location: "SF Bay Area", interests: "golfing, karate, cooking", good_standing: true, availability: DateTime.now, other: "")
+r2 = Mentor.create!(first_name: "Chris", last_name: "Samson",email: "chris@mail.com", years_of_experience: 4, current_company: "Acme Inc.", current_industry: "FinTech", linkedin_link: "linkedin.com/chris", race: "White", gender: "male", socioeconomic_background: "middle", orientation: "straight", location: "SF Bay Area", interests: "tennis, wind-surfing", good_standing: true, availability: DateTime.now, other: "")
+r3 = Mentor.create!(first_name: "Andrea", last_name: "James",email: "a@mail.com", years_of_experience: 5, current_company: "DBC", current_industry: "Education", linkedin_link: "linkedin.com/andrea", race: "Asian", gender: "female", socioeconomic_background: "lower", orientation: "bisexual", location: "SF Bay Area", interests: "golfing, karate, cooking", good_standing: true, availability: DateTime.now, other: "")
+
+
 
 t = Mentee.create!(first_name: "Jane", last_name: "Thomas",email: "j@mail.com", objective: "code reviews", linkedin_link: "linkedin.com/jane", race: "Asian", gender: "female", socioeconomic_background: "middle", orientation: "straight", location: "SF Bay Area", interests: "beanie baby collection", good_standing: true, mentor_demographic_pref: true, other: "")
 
