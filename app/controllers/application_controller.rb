cada3f83dcfa35b945fa22a836d498c2cb97f28b
Rebecca Orr <wholemilklover@gmail.com>
2017-02-28 11:33:18 -0800

    Fixes signout (again)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,5 +7,9 @@ class ApplicationController < ActionController::Base
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
 
+  def set_auth
+    @auth = session[:omniauth] if session[:omniauth]
+  end
+
   helper_method :current_user
 end
