8772a0eb5ba5172f01a854a69d45ac25edd3a742
Rebecca Orr <wholemilklover@gmail.com>
2017-03-01 07:57:25 -0800

    Minor changes to get matches.

--- app/models/match.rb
+++ app/models/match.rb
@@ -8,13 +8,15 @@ class Match < ApplicationRecord
 # is called
 
   def mentor_matches(mentee)
-    mentors = Mentor.all 
+    mentors = Mentor.all.shuffle
     matches = []
+    count = mentors.count
     
     # Coding out while loop for faster debugging purposes, 
     # though for refactor it's important to consider cases
     # where no matches are found
     # while matches.length <= 1
+    until matches.length == 3 || count < 1 do
       mentors.each do |mentor|
         if mentor.race == mentee.race
           matches.push(mentor)
@@ -25,8 +27,9 @@ class Match < ApplicationRecord
         elsif mentor.orientation == mentee.orientation
           matches.push(mentor)
         end
+      count -= 1
       end
-    # end
+    end
     matches
   end
 end
