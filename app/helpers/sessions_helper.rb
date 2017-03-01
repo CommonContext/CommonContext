a810437feaac2772b39eb4a17fd2cbde0dc50c27
Rebecca Orr <wholemilklover@gmail.com>
2017-02-28 22:56:45 -0800

    Adds get_user method to return mentor or mentee.

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,7 +1,17 @@
 module SessionsHelper
 
- def current_user
+  def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def get_user 
+    if user_type == "mentor"
+      @this_user = Mentor.find_by(user_id: session[:user_id])
+    elsif user_type == "mentee"
+      @this_user = Mentee.find_by(user_id: session[:user_id])
+    else
+      route_user
+    end
   end
 
   def set_user_type(type)
