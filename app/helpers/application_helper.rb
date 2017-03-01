8a1a659e38300c39e4c40951cc25267591015bae
Dharini Murugaprabhu <dharinishree@gmail.com>
2017-03-01 15:36:20 -0800

    trying to add random images in background

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def randomized_background_image
+    images = ["assets/street2.png", "/assets/people.jpg"]
+    images[rand(images.size)]
+  end
 end
+
+    # background-image: url(<%= randomized_background_image %>) no-repeat center center fixed; 
+    # <% environment.context_class.instance_eval { include randomized_background_image } %>
